52042faeafbe939f36c48ac90ea06a206f83e5aa
imlaamer <lolitayenik@ukr.net>
2023-12-07 21:59:49 +0200

    1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
+import { userData } from "./Profile/user";
+import { Profile } from "./Profile/Profile";
+
+
+
 export const App = () => {
+  const userStats = {
+     followers: 5603,
+    views: 4827,
+    likes: 1308 
+  }
+  
   return (
     <div
       style={{
@@ -7,10 +18,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username="Jacques Gluke"
+        tag="jgluke"
+        location="Ocho Rios, Jamaica"
+        // avatar="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
+        stats={userStats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import css from "./Profile.module.css"
+
+console.log(css)
+
+export function Profile({
+  username,
+  tag,
+  location,
+  avatar = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: { followers, views, likes },
+}) {
+  return (
+      <div className={css.profile}>
+          <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
